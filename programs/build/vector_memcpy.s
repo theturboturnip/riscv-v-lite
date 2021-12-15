6c398190006f8c23d6ae7a1983bf08939f384fb3
theturboturnip <popgoestoast@gmail.com>
2021-12-15 23:33:56 +0000

    Made vector_memcpy actually do a vector memcpy

--- programs/build/vector_memcpy.s
+++ programs/build/vector_memcpy.s
@@ -2,32 +2,86 @@
 	.attribute	4, 16
 	.attribute	5, "rv32i2p0_m2p0_a2p0_f2p0_d2p0_v0p10_zvlsseg0p10"
 	.file	"vector_memcpy.c"
-	.globl	fib
+	.globl	vector_memcpy_e8
 	.p2align	2
-	.type	fib,@function
-fib:
-	addi	sp, sp, -16
-	sw	ra, 12(sp)
-	sw	s0, 8(sp)
-	sw	s1, 4(sp)
-	addi	a1, zero, 2
-	mv	s0, a0
-	blt	a0, a1, .LBB0_2
-	addi	a0, s0, -1
-	call	fib
-	mv	s1, a0
-	addi	a0, s0, -2
-	call	fib
-	add	s0, a0, s1
+	.type	vector_memcpy_e8,@function
+vector_memcpy_e8:
+	beqz	a0, .LBB0_2
+.LBB0_1:
+	vsetvli	a3, a0, e32, m8, ta, mu
+	vle32.v	v8, (a1)
+	vse32.v	v8, (a2)
+	slli	a4, a3, 2
+	add	a1, a1, a4
+	sub	a0, a0, a3
+	add	a2, a2, a4
+	bnez	a0, .LBB0_1
 .LBB0_2:
-	mv	a0, s0
-	lw	s1, 4(sp)
-	lw	s0, 8(sp)
-	lw	ra, 12(sp)
-	addi	sp, sp, 16
 	ret
 .Lfunc_end0:
-	.size	fib, .Lfunc_end0-fib
+	.size	vector_memcpy_e8, .Lfunc_end0-vector_memcpy_e8
+
+	.globl	vector_memcpy_harness
+	.p2align	2
+	.type	vector_memcpy_harness,@function
+vector_memcpy_harness:
+	addi	sp, sp, -32
+	sw	ra, 28(sp)
+	sw	s0, 24(sp)
+	sw	s1, 20(sp)
+	sw	s2, 16(sp)
+	sw	s3, 12(sp)
+	mv	a0, zero
+	lui	a1, %hi(vector_memcpy_harness.data)
+	addi	a1, a1, %lo(vector_memcpy_harness.data)
+	lui	a2, 1
+	addi	a2, a2, -2048
+.LBB1_1:
+	sw	a0, 0(a1)
+	addi	a0, a0, 1
+	addi	a1, a1, 4
+	bne	a0, a2, .LBB1_1
+	lui	s2, %hi(vector_memcpy_harness.data)
+	addi	s0, s2, %lo(vector_memcpy_harness.data)
+	lui	s3, %hi(vector_memcpy_harness.out_data)
+	addi	s1, s3, %lo(vector_memcpy_harness.out_data)
+	lui	a0, 1
+	addi	a0, a0, -2048
+	mv	a1, s0
+	mv	a2, s1
+	call	vector_memcpy_e8
+	lw	a0, %lo(vector_memcpy_harness.data)(s2)
+	lw	a1, %lo(vector_memcpy_harness.out_data)(s3)
+	bne	a0, a1, .LBB1_7
+	mv	a4, zero
+	addi	a0, s0, 4
+	addi	a1, s1, 4
+	addi	a3, zero, 2047
+.LBB1_4:
+	mv	a2, a4
+	beq	a4, a3, .LBB1_6
+	lw	a5, 0(a0)
+	lw	s1, 0(a1)
+	addi	a0, a0, 4
+	addi	a1, a1, 4
+	addi	a4, a2, 1
+	beq	a5, s1, .LBB1_4
+.LBB1_6:
+	addi	a0, zero, 2046
+	sltu	a0, a0, a2
+	j	.LBB1_8
+.LBB1_7:
+	mv	a0, zero
+.LBB1_8:
+	lw	s3, 12(sp)
+	lw	s2, 16(sp)
+	lw	s1, 20(sp)
+	lw	s0, 24(sp)
+	lw	ra, 28(sp)
+	addi	sp, sp, 32
+	ret
+.Lfunc_end1:
+	.size	vector_memcpy_harness, .Lfunc_end1-vector_memcpy_harness
 
 	.globl	main
 	.p2align	2
@@ -35,16 +89,23 @@ fib:
 main:
 	addi	sp, sp, -16
 	sw	ra, 12(sp)
-	addi	a0, zero, 8
-	call	fib
+	call	vector_memcpy_harness
 	lui	a1, 983040
 	sw	a0, 0(a1)
 	lw	ra, 12(sp)
 	addi	sp, sp, 16
 	ret
-.Lfunc_end1:
-	.size	main, .Lfunc_end1-main
+.Lfunc_end2:
+	.size	main, .Lfunc_end2-main
 
+	.type	vector_memcpy_harness.data,@object
+	.local	vector_memcpy_harness.data
+	.comm	vector_memcpy_harness.data,8192,4
+	.type	vector_memcpy_harness.out_data,@object
+	.local	vector_memcpy_harness.out_data
+	.comm	vector_memcpy_harness.out_data,8192,4
 	.ident	"Ubuntu clang version 13.0.1-++20211215022811+5932c004778c-1~exp1~20211215022819.27"
 	.section	".note.GNU-stack","",@progbits
 	.addrsig
+	.addrsig_sym vector_memcpy_harness.data
+	.addrsig_sym vector_memcpy_harness.out_data
--- programs/src/vector_memcpy.c
+++ programs/src/vector_memcpy.c
@@ -1,16 +1,54 @@
-int fib(const int a)
-{
-  if(a<2)
-    return a;
-  else
-    return fib(a-1) + fib(a-2);
+// int fib(const int a)
+// {
+//   if(a<2)
+//     return a;
+//   else
+//     return fib(a-1) + fib(a-2);
+// }
+
+#include <riscv_vector.h>
+#include <stdint.h>
+
+// n = number of elements to copy
+// in = pointer to data (should be aligned to 128-bit?)
+// out = pointer to output data (should be aligned?)
+void vector_memcpy_e8(size_t n, const int32_t* __restrict__ in, const int32_t* __restrict__ out) {
+    size_t copied_per_iter = 0;
+    for (; n > 0; n -= copied_per_iter) {
+        copied_per_iter = vsetvl_e32m8(n);
+        // copied_per_iter is included in the intrinsic, not because it changes the actual instruction,
+        // but if you wanted to change it it would do vsetvl to set architectural state
+        vint32m8_t data = vle32_v_i32m8(in, copied_per_iter);
+        vse32_v_i32m8(out, data, copied_per_iter);
+
+        in += copied_per_iter;
+        out += copied_per_iter;
+    }
+}
+
+int vector_memcpy_harness() {
+    static int data[2048] = {0};
+    static int out_data[2048] = {0};
+
+    for (int i = 0; i < 2048; i++) {
+        data[i] = i;
+    }
+
+    vector_memcpy_e8(2048, data, out_data);
+
+    for (int i = 0; i < 2048; i++) {
+        if (data[i] != out_data[i]) {
+            return 0;
+        }
+    }
+    return 1;
 }
 
 int main(void)
 {
   int *outputDevice = (int*) 0xf0000000; // magic output device
   int result;
-  result = fib(8);
+  result = vector_memcpy_harness();
   outputDevice[0] = result;
   return result;
 }
