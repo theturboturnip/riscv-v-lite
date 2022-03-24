9351c7028929ad7bfaa9aec0671bffd0e20b91e7
Samuel Stark <dev+samuel.winslow.stark@gmail.com>
2022-03-24 18:27:17 +0000

    Made SafeTaggedCap Debug

--- rsim/src/processor/elements/cheri/capability.rs
+++ rsim/src/processor/elements/cheri/capability.rs
@@ -2,7 +2,7 @@ pub use rust_cheri_compressed_cap::{CompressedCapability, Cc128, Cc128Cap, wrapp
 
 /// Enumeration that stores either raw data or a valid capability.
 /// The [Cc128Cap] struct has its own tag bit - this will always be True *as long as [SafeTaggedCap::ValidCap] is not created manually*.
-#[derive(Copy,Clone,PartialEq,Eq)]
+#[derive(Debug,Copy,Clone,PartialEq,Eq)]
 pub enum SafeTaggedCap {
     RawData{ top: u64, bot: u64 },
     ValidCap(Cc128Cap)
