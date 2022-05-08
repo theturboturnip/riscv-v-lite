b947c878a5b63d06ad1db465baca7d12561a4cf7
Samuel Stark <samuel.winslow.stark+dev@gmail.com>
2022-05-08 18:38:43 +0100

    Renamed cheri toolchains for consistency with arch strings

--- rsim/test.sh
+++ rsim/test.sh
@@ -15,10 +15,10 @@ cargo run direct rv64imv ../programs/build/llvm-trunk-rv64imv/vector_memcpy/mem.
 cargo run direct rv64imv ../programs/build/llvm-13-rv64imv/hello_world/mem.bin
 cargo run direct rv64imv ../programs/build/llvm-13-rv64imv/vector_memcpy_pointers/mem.bin
 
-cargo run direct rv64imvxcheri ../programs/build/llvm-13-rv64imxcheri/vector_memcpy/vector_memcpy.elf
-cargo run direct rv64imvxcheri ../programs/build/llvm-13-rv64imxcheri/hello_world/hello_world.elf
-cargo run direct rv64imvxcheri ../programs/build/llvm-13-rv64imxcheri/vector_memcpy_pointers/vector_memcpy_pointers.elf
+cargo run direct rv64imvxcheri ../programs/build/llvm-13-rv64imvxcheri/vector_memcpy/vector_memcpy.elf
+cargo run direct rv64imvxcheri ../programs/build/llvm-13-rv64imvxcheri/hello_world/hello_world.elf
+cargo run direct rv64imvxcheri ../programs/build/llvm-13-rv64imvxcheri/vector_memcpy_pointers/vector_memcpy_pointers.elf
 
-cargo run direct rv64imvxcheri-int ../programs/build/llvm-13-rv64imxcheri-int/vector_memcpy/vector_memcpy.elf
-cargo run direct rv64imvxcheri-int ../programs/build/llvm-13-rv64imxcheri-int/hello_world/hello_world.elf
-cargo run direct rv64imvxcheri-int ../programs/build/llvm-13-rv64imxcheri-int/vector_memcpy_pointers/vector_memcpy_pointers.elf
+cargo run direct rv64imvxcheri-int ../programs/build/llvm-13-rv64imvxcheri-int/vector_memcpy/vector_memcpy.elf
+cargo run direct rv64imvxcheri-int ../programs/build/llvm-13-rv64imvxcheri-int/hello_world/hello_world.elf
+cargo run direct rv64imvxcheri-int ../programs/build/llvm-13-rv64imvxcheri-int/vector_memcpy_pointers/vector_memcpy_pointers.elf
